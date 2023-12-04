a3e887f085c04edaaa086655e715df4de4de6ba4
stefano.bozzoni <stefano.bozzoni@gmail.com>
2023-12-04 20:31:29 +0100

    Migration from groovy to kotlin dsl build.gradle

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,12 @@
+object libVersions {
+    const val okHttp = "4.9.2"
+    const val retrofit = "2.9.0"
+    const val room = "2.5.2"
+    const val paging_compose = "3.2.1"
+    const val navigation_compose = "2.6.0"
+    const val lifecycle_compose = "2.6.2"
+    const val coil_compose = "2.4.0"
+    const val jetpack_core = "1.8.21"
+    const val koin_compose = "3.4.3"
+    const val activity_compose = "1.7.2"
+}
